fc44611a4b4c55a0138705fca0b7d2da34135b4a
Alex Demidov <a.demidov13@gmail.com>
2020-08-12 17:48:45 +0300

    transerred locators, moved methods to basepage

--- pages/locators.py
+++ pages/locators.py
@@ -1,18 +1,23 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-class LoginPageLocators():
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
 
 
-class ProductPageLocators():
+class ProductPageLocators:
     BASKET_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
     PRODUCT_NAME = (By.CSS_SELECTOR, 'div.product_main h1')
-    BASKET_ADDED_MESSAGE = (By.CSS_SELECTOR, 'div.alertinner')
+    BASKET_ADDED_MESSAGE = (By.CSS_SELECTOR, '.alert-success .alertinner strong')
     BASKET_TOTAL = (By.CSS_SELECTOR, '.alert-info .alertinner strong')
     PRODUCT_PRICE = (By.CSS_SELECTOR, '.col-sm-6 .price_color')
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, '#login_link_inc')
--- pages/product_page.py
+++ pages/product_page.py
@@ -14,11 +14,20 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), 'There is no product name'
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         message = self.browser.find_element(*ProductPageLocators.BASKET_ADDED_MESSAGE).text
-        assert product_name in message, 'Product name is not in the message'
+        assert product_name == message, 'Product name is not in the message'
 
     def should_be_basket_total(self):
         assert self.is_element_present(*ProductPageLocators.BASKET_TOTAL), 'There is no basket total price'
         assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), 'There is no product price'
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         total_price = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
-        assert product_price in total_price, 'Total price doesnt match the product price'
+        assert product_price == total_price, 'Total price doesnt match the product price'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.BASKET_ADDED_MESSAGE),\
+            'Success message is presented but shouldnt'
+
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.BASKET_ADDED_MESSAGE),\
+            "Success message didn't disappear"
+
